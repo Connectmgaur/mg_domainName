84cbb4899d2dbef21c5a8b9a774ea9c2e9cf6a74
Meenakshi Gaur <136475194+Connectmgaur@users.noreply.github.com>
2023-08-11 23:20:05 +0000

    Code for domain name

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-/* eslint-disable */
+/* eslint-disable 
 import "bootstrap";
 import "./style.css";
 
@@ -7,6 +7,21 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello World!");
-};
+  //console.log("Hello World!");
 /* eslint-disable */
+
+let pronouns = ["the", "our"];
+let adjectives = ["great", "big"];
+let nouns = ["jogger", "racoon"];
+
+function generateDomainNames() {
+  for (let i = 0; i < pronouns.length; i++) {
+    for (let j = 0; j < adjectives.length; j++) {
+      for (let k = 0; k < nouns.length; k++) {
+        let domainName = pronouns[i] + adjectives[j] + nouns[k] + ".com";
+        console.log(domainName);
+      }
+    }
+  }
+}
+generateDomainNames();
