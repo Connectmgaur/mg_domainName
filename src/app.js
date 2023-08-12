a937403291f5bef9de2ef2ed7f36598d882b05d7
Meenakshi Gaur <136475194+Connectmgaur@users.noreply.github.com>
2023-08-12 00:10:07 +0000

    check

--- src/app.js
+++ src/app.js
@@ -1,34 +1,16 @@
-/* eslint-disable 
-import "bootstrap";
-import "./style.css";
+let pronouns = ["the", "our"];
+let adjectives = ["great", "big"];
+let nouns = ["jogger", "racoon"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  //console.log("Hello World!");
-/* eslint-disable */
-
-/* eslint-disable */
-window.onload = function() {
-  let pronouns = ["the", "our"];
-  let adjectives = ["great", "big"];
-  let nouns = ["jogger", "racoon"];
-
-  function generateDomainNames() {
-    let outputElement = document.getElementById("output");
-
-    for (let i = 0; i < pronouns.length; i++) {
-      for (let j = 0; j < adjectives.length; j++) {
-        for (let k = 0; k < nouns.length; k++) {
-          let domainName =
-            pronouns[i] + adjectives[j] + nouns[k] + ".com" + ".ie";
-          outputElement.innerHTML += domainName + "<br>";
-        }
+function generateDomainNames() {
+  for (let i = 0; i < pronouns.length; i++) {
+    for (let j = 0; j < adjectives.length; j++) {
+      for (let k = 0; k < nouns.length; k++) {
+        let domainName = pronouns[i] + adjectives[j] + nouns[k] + ".com";
+        console.log(domainName);
       }
     }
   }
+}
 
-  generateDomainNames();
-};
+generateDomainNames();
